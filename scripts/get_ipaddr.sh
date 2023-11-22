ee4ee7e4a3a5fd6e53fdc89e18a22a4c2bd7ea8e
david072 <david.g.ganz@gmail.com>
2023-11-22 07:12:10 +0100

    get_ipaddr.sh: work for MacOS and Linux

--- scripts/get_ipaddr.sh
+++ scripts/get_ipaddr.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-echo $(ifconfig enp37s0 | grep "inet" | cut -d: -f2 | awk '{print $2}')
+ipaddr_linux () {
+  echo $(ifconfig enp37s0 | grep "inet" | cut -d: -f2 | awk '{print $2}')
+}
+
+ipaddr_mac () {
+  echo $(ipconfig getifaddr en0)
+}
+
+case $(uname -s) in
+  Linux) ipaddr_linux
+  ;;
+  Darwin) ipaddr_mac
+  ;;
+  *) echo "Unrecognized platform"
+  ;;
+esac
+
